d9791b941d03909224c18c37f4ef78a4b8346c9d
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-10-11 01:42:50 +0000

    ast

--- src/CompileTime/AST.fs
+++ src/CompileTime/AST.fs
@@ -5,16 +5,28 @@ type NodeType =
   | Num
   | EOF
 
+[<StructuredFormatDisplay("line: {line}, colmun: {colmun}")>]
 type Expr =
-  abstract member Type : NodeType
+  abstract member Type : NodeType with get
   abstract member line : int with get, set
   abstract member colmun : int with get, set
-
-type Program =
-  inherit Expr
-  abstract member Type : NodeType
+[<AbstractClass>]
+[<StructuredFormatDisplay("got program => body: {body}, line: {line}, colmun: {colmun}")>]
+type Program(aline : int, acolmun : int) =
+  interface Expr with
+    member val line = aline with get, set
+    member val colmun = acolmun with get, set
+    member this.Type = NodeType.Program
+  end
   abstract member body : Expr list with get, set
 
-type Num = 
-  inherit Expr
+[<AbstractClass>]
+[<StructuredFormatDisplay("got num => value: {value}, line: {line}, colmun: {colmun}")>]
+type Num(aline : int, acolmun : int ) = 
+  interface Expr with
+    member val line = aline with get, set
+    member val colmun = acolmun with get, set
+
+    member this.Type : NodeType = NodeType.Num
+  end
   abstract member value : float with get, set
