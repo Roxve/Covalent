258835211ae6eb61c82977f95922a63b888b158a
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-10-20 02:18:26 +0000

    spilt interpreter

--- src/Runtime/interpreter.fs
+++ src/Runtime/interpreter.fs
@@ -5,24 +5,7 @@ open AtomicLang.AST
 
 module Interpreter=
   type Interpreter(code : string)=
-    let parser = new Parser(code)
-    let mutable line = 1
-    let mutable colmun = 0
-    do parser.productAST() |> ignore
-
-    let update =
-      line <- parser.current_node.line
-      colmun <- parser.current_node.colmun
-    member private x.at() = 
-      update
-      parser.current_node
-     
-    member private x.take() =
-      let prev = x.at()
-      parser.productAST() |> ignore
-      update
-      prev
-    
+    inherit InterpreterBinary.Interpreter(code)
     member x.run()=
       let mutable lastrun: RuntimeVal = new NullVal();
       while not (x.at().Type = AST.NodeType.EOP) do
--- src/Runtime/interpreter_binary_expr.fs
+++ src/Runtime/interpreter_binary_expr.fs
@@ -1,1 +1,4 @@
 namespace AtomicLang
+module InterpreterBinary=
+  type Interpreter(code : string)=
+    inherit InterpreterMain.Interpreter(code)
