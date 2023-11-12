797411f7a9e19ce82a21463651b0547658a97cff
void* <116169309+ptVoid@users.noreply.github.com>
2023-11-12 01:12:11 +0000

    parser

--- src/compile/AST.nim
+++ src/compile/AST.nim
@@ -1,5 +1,6 @@
 type
   NodeType* = enum
+    Program,
     Num,
     ID,
     Str,
@@ -7,16 +8,19 @@ type
     binaryExpr,
     Operator,
 
-  Expr* = object of RootObj
+  Expr* = ref object of RootObj
     node*: NodeType
     line*, colmun*: int
+  Prog* = ref object of Expr
+    body*: seq[Expr]
+
   IDVal* = ref object of Expr
     symbol*: string
   OperatorVal* = ref object of Expr
     symbol*: string
 
   NumVal* = ref object of Expr
-    value*: int | float
+    value*: float
   StrVal* = ref object of Expr
     value: string
 
@@ -25,17 +29,29 @@ type
     right: Expr
     operator: OperatorVal
 
+proc Make_Prog*(body: seq[Expr], line: int, colmun: int): Prog =
+  return Prog(node: NodeType.Program, line: line, colmun: colmun)
+
+
 proc Make_ID*(symbol: string, line: int, colmun: int): IDVal =
   return IDVal(node: NodeType.ID, symbol: symbol, line: line, colmun: colmun)
+
+
 
 proc Make_Operator*(symbol: string, line: int, colmun: int): OperatorVal =
   return OperatorVal(node: NodeType.ID, symbol: symbol, line: line, colmun: colmun)
 
-proc Make_Num*(value: int | float, line: int, colmun: int): NumVal =
+
+
+proc Make_Num*(value: float, line: int, colmun: int):  NumVal =
   return NumVal(node: NodeType.ID, value: value, line: line, colmun: colmun)
+
+
 
 proc Make_Str*(value: string, line: int, colmun: int): StrVal =
   return StrVal(node: NodeType.ID, value: value, line: line, colmun: colmun)
+
+
 
 proc Make_BinaryExpr*(left: Expr, right: Expr, operator: OperatorVal, line: int, colmun: int): BinaryExpr =
   return BinaryExpr(node: NodeType.ID, left: left,right: right, operator: operator, line: line, colmun: colmun)
--- src/compile/parse_main.nim
+++ src/compile/parse_main.nim
@@ -2,6 +2,7 @@ import tokenize
 import AST
 import options
 
+
 type
   ScopeType = enum
     top,
@@ -13,30 +14,30 @@ type
   Scope = ref object
     parent: Option[Scope]
     Type: ScopeType
-  Parser = object
-    line, colmun: int
+  Parser* = object
+    line*, colmun*: int
     tokenizer: Tokenizer
     last_token: Token
-    current_scope: Scope
+    current_scope*: Scope
     current_node: Expr
-proc mk_scope(Type: ScopeType, parent: Option[Scope]): Scope =
+proc mk_scope*(Type: ScopeType, parent: Option[Scope]): Scope =
   return Scope(parent: parent, Type: Type)
 
-proc make_parser(src: string): Parser =
+proc make_parser*(src: string): Parser =
   var parser = Parser(line: 1, colmun: 0, tokenizer: make_tokenizer(src))
   parser.last_token = parser.tokenizer.next()
   parser.current_scope = mk_scope(ScopeType.top, none(Scope))
   return parser
 
-proc update(this: var Parser) =
+proc update*(this: var Parser) =
   this.line = this.tokenizer.line
   this.colmun = this.tokenizer.colmun
 
-proc at(this: var Parser): Token =
+proc at*(this: var Parser): Token =
   this.update
   return this.tokenizer.current_token
 
-proc take(this: var Parser): Token =
+proc take*(this: var Parser): Token =
   var prev = this.at
   this.last_token = prev
   discard this.tokenizer.next()
