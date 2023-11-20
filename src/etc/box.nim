8170511459d060e2786f2b8f733b75e40104813f
void* <116169309+ptVoid@users.noreply.github.com>
2023-11-20 17:44:58 +0000

    cool terminal boxes making it a repo fr

--- src/etc/box.nim
+++ src/etc/box.nim
@@ -1,34 +1,42 @@
 import strutils
+import sugar
 
-echo  "╭───╮│─╯╰─"
+proc green*(s: string): string = "\e[32m" & s & "\e[0m"
+proc grey*(s: string): string = "\e[90m" & s & "\e[0m"
+proc yellow*(s: string): string = "\e[33m" & s & "\e[0m"
+proc red*(s: string): string = "\e[31m" & s & "\e[0m"
+proc none*(s: string): string = s
+
 # some how it works idk how
-proc makeBox*(text: string, title: string): string =
+proc makeBox*(text: string, title: string = "", full_style: (s: string) -> string = none, border_style: (s: string) -> string = none, text_style: (s: string) -> string = none): string =
   var items: seq[string] = text.split("\n")
-  var maxWidth = int(title.len)
-  result &= "╭"
+  var maxWidth = title.len + 4
+  result &= "╭".border_style
   for item in items:
-    if int(item.len) > maxWidth - 4: maxWidth = int(item.len) + 4
+    if int(item.len) > maxWidth - 4: maxWidth = item.len + 4
   var i = 0
-  while i < int(maxWidth):  
-    if i == int(maxWidth / 2) - 3: 
-      result &= title 
-      i = i + int(title.len) + 2
+  while i < maxWidth:  
+    if i == int(maxWidth / 2) - title.len + 2: 
+      result &= title.border_style 
+      i = i + title.len + 2
       continue
-    result &= "─"
+    result &= "─".border_style
     inc i
-  result &= "╮\n"
+  result &= "╮\n".border_style
 
   for item in items:
-    var res = "│ " & item 
-    while int(res.len) < maxWidth:
+    var res = "│ " & item.text_style
+    while res.len < maxWidth + text_style("").len:
       res &= " "
-    res &= " │\n"
-    result &= res
+    res &= " │\n".border_style 
+    result &= res.border_style
 
-  result &= "╰─"
-  for i in title.len .. maxWidth: 
-    result &= "─"
-  result &= "─╯"
+  result &= "╰─".border_style
+  for i in 5 .. maxWidth: 
+    result &= "─".border_style
+  result &= "─╯".border_style
+  result = result.full_style
 
+echo makeBox("error unknown op got left 9292929 right 889299 op ? \nat idk where maybe 5:6\najjaiaiakakaioakakakiaia", "error", border_style=red, text_style=yellow)
+echo "windows moment realll window".makeBox("window its cool")
 
-echo makeBox("error unknown op got left 9292929 right 889299 op ? \nat idk where maybe 5:6\najjaiaiakakaioakakakiaia", "error")
