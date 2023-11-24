df2a6bc173c5a3ae93ea011dada50634e3ff9064
ptVoid <116169309+ptVoid@users.noreply.github.com>
2023-11-24 08:57:25 +0000

    moved codegen from codegen.nim to AST for each node

--- src/compile/AST.nim
+++ src/compile/AST.nim
@@ -2,6 +2,10 @@ import codegen_def
 import ../etc/enviroments
 import noxen
 import strformat
+import ../etc/utils
+import Options
+import math
+
 
 type
   NodeType* = enum
@@ -18,7 +22,7 @@ type
 
   Expr* = ref object 
     line*, colmun*:int
-    codegen: proc(self: var Codegen): ValueType
+    codegen*: proc(self : var Codegen): ValueType
     case kind*: NodeType 
     of Program: 
       body*: seq[Expr] 
@@ -45,6 +49,10 @@ type
     else:
       discard
 
+template NodeCodegen(code: untyped) =
+  expr.codegen = proc(self {.inject.}: var Codegen): ValueType =
+    code
+
 proc MakeError*(msg: string, line, colmun: int): Expr=
   return Expr(kind: Error, msg: msg,line: line, colmun: colmun)
 
@@ -54,9 +62,16 @@ proc MakeProg*(body: seq[Expr], line: int, colmun: int): Expr =
 
 
 proc MakeID*(symbol: string, line: int, colmun: int): Expr =
-  return Expr(kind:  NodeType.ID, symbol: symbol, line: line, colmun: colmun)
-
-
+  var expr = Expr(kind:  NodeType.ID, symbol: symbol, line: line, colmun: colmun)
+  NodeCodegen:
+      var name = expr.symbol
+      var index = self.env.getVarIndex(name)
+      if index == 0:
+        return
+      result = ValueType.int
+      self.body.emit(OP_LOADNAME, reg, int16(index).to2Bytes)
+      reg += 1
+  return expr
 
 proc MakeOperator*(symbol: string, line: int, colmun: int): Expr =
   return Expr(kind:  NodeType.Operator, op: symbol, line: line, colmun: colmun)
@@ -64,18 +79,32 @@ proc MakeOperator*(symbol: string, line: int, colmun: int): Expr =
 
 
 proc MakeNum*(value: float, line: int, colmun: int):  Expr =
-  return Expr(kind:  NodeType.Num, num_value: value, line: line, colmun: colmun)
+  var expr =  Expr(kind:  NodeType.Num, num_value: value, line: line, colmun: colmun)
+  NodeCodegen:  
+      var count = int16(0)
+      if expr.num_value == round(expr.num_value):
+        result = ValueType.int        
+        count = self.addConst(TAG_INT, ValueType.int, uint32(expr.num_value).to4Bytes())
+      # LOAD dist imm
+      self.body.emit(OP_LOAD_CONST, reg, count.to2Bytes)
+      reg += 1  
+  return expr
 
 
 
 proc MakeStr*(value: string, line: int, colmun: int): Expr =
-  return Expr(kind:  NodeType.Str, str_value: value, line: line, colmun: colmun)
+  var expr = Expr(kind:  NodeType.Str, str_value: value, line: line, colmun: colmun)
+  NodeCodegen:
+      result = ValueType.str
+      var count = self.addConst(TAG_STR, result,int16(expr.str_value.len), expr.str_value.StrToBytes)
 
+      self.body.emit(OP_LOAD_CONST, reg, count.to2Bytes)
+      reg += 1
 
 
 proc MakeBinaryExpr*(left: Expr, right: Expr, operator: Expr, line: int, colmun: int): Expr =
   var expr = Expr(kind:  NodeType.binaryExpr, left: left,right: right, operator: operator, line: line, colmun: colmun)
-  expr.codegen = proc(self: var Codegen): ValueType =              
+  NodeCodegen:              
       var L = expr.left
       var R = expr.right
       var binop = expr.operator.op
@@ -105,13 +134,41 @@ proc MakeBinaryExpr*(left: Expr, right: Expr, operator: Expr, line: int, colmun:
       # store results of math into reg - 2 ex (8 + 8 + 8) ADD R0 R0 R1 then ADD R0 R0 R1
       reg -= 1
   return expr
+
+
+
+
 proc MakeVarDeclartion*(name: string, value: Expr, line, colmun: int): Expr =
-  return Expr(kind: varDeclare, declare_name: name, declare_value: value, line: line, colmun: colmun)
+  var expr = Expr(kind: varDeclare, declare_name: name, declare_value: value, line: line, colmun: colmun)
+  NodeCodegen:
+      var name = expr.declare_name
+      if self.env.resolve(name) != none(Enviroment):
+        return
+      self.env.addVarIndex(name)
+      
+      var val = expr.declare_value
+      result = val.codegen(self)
+      # DIST_INDEX <= REG
+      self.body.emit(OP_STRNAME, int16(self.env.var_count).to2Bytes(), reg - 1)
+      reg -= 1
+
+  return expr
+
 
 
 proc MakeVarAssignment*(name: string, value: Expr, line, colmun: int): Expr =
-  return Expr(kind: varAssign, assign_name: name, assign_value: value, line: line, colmun: colmun)
-
+  var expr = Expr(kind: varAssign, assign_name: name, assign_value: value, line: line, colmun: colmun)
+  NodeCodegen:
+      var name = expr.assign_name
+      var index = self.env.getVarIndex(name)
+      if index == 0:
+        return  
+      var val = expr.assign_value
+      result = val.codegen(self)
+      # DIST_INDEX <= REG
+      self.body.emit(OP_STRNAME, int16(index).to2Bytes(), reg - 1)
+      reg += 1
+  return expr
 
 proc error(self: Codegen, msg: string) =
   echo makeBox(msg & &"\nat line:{self.line}, colmun:{self.colmun}", "error", full_style=red)
