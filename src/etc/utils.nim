a2c5cd9138757afbfab82ef1bf0c6000a3f6783a
ptVoid <116169309+ptVoid@users.noreply.github.com>
2023-11-24 22:30:45 +0000

    stuff

--- src/etc/utils.nim
+++ src/etc/utils.nim
@@ -1,14 +1,18 @@
 import unicode
 import sequtils
+type intBytes = array[0..3, byte]
+type intVal = uint32
 
-proc to4Bytes*(val: int | uint32 | int32): seq[byte] =
-    var bytes: seq[byte] = @[]
-    bytes.add(byte((val shr 24) and 0xFF))
-    bytes.add(byte((val shr 16) and 0xFF))
-    bytes.add(byte((val shr 8) and 0xFF))
-    bytes.add(byte(val and 0xFF))
+proc to4Bytes*(input: int | uint32 | int32): seq[byte] =
+    var bytes: seq[byte] = toSeq(cast[intBytes](input))
     return bytes
 
+proc seqToIntBytes*(val: seq[byte]): intBytes =
+  result = [byte(0), 0, 0,0]
+  if val.len > 0:
+    for i in 0..val.len - 1:
+      result[i] = val[i]
+  
 proc to2Bytes*(val: int16): seq[byte] =
     var bytes: seq[byte] = @[]
     bytes.add(byte((val shr 8) and 0xFF))
@@ -23,18 +27,7 @@ proc signExtend*(x: uint8): uint32 =
     result = res
 
 proc makeInt*(x: seq[byte]): uint32 =
-  if x.len == 2:
-    return
-           signExtend(x[0] shl 8) or
-           signExtend(x[1])
-  elif x.len == 1:
-    return signExtend(x[0])
-  else:
-    return
-         signExtend(x[0] shl 24) or
-         signExtend(x[1] shl 16) or
-         signExtend(x[2] shl 8) or
-         signExtend(x[3])
+  result = uint32(cast[intVal](x.seqToIntBytes))
 
 
 
