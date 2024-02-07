11301c6421e1a7d211994e2f19317a2373766107
Roxve <116169309+Roxve@users.noreply.github.com>
2024-02-07 08:38:16 +0000

    some upgrades

---
+++ src/codegen/tools.rs
@@ -0,0 +1,87 @@
+use inkwell::types::{AnyTypeEnum, BasicTypeEnum};
+
+use inkwell::values::{BasicValue, BasicValueEnum};
+
+use crate::source::{ErrKind, Source};
+
+pub fn any_type_to_basic(ty: AnyTypeEnum) -> BasicTypeEnum {
+    match ty {
+        AnyTypeEnum::PointerType(p) => BasicTypeEnum::PointerType(p),
+        AnyTypeEnum::IntType(i) => BasicTypeEnum::IntType(i),
+        AnyTypeEnum::FloatType(f) => BasicTypeEnum::FloatType(f),
+        AnyTypeEnum::ArrayType(t) => BasicTypeEnum::ArrayType(t),
+        _ => todo!(),
+    }
+}
+
+pub fn get_type_name(ty: BasicTypeEnum) -> String {
+    match ty {
+        BasicTypeEnum::IntType(_) => "_int".to_string(),
+        BasicTypeEnum::FloatType(_) => "_float".to_string(),
+        BasicTypeEnum::ArrayType(t) => {
+            "_arr_".to_owned() + get_type_name(t.get_element_type()).as_str()
+        }
+        BasicTypeEnum::PointerType(ref t) => {
+            let el = t.get_element_type();
+            let bel = &any_type_to_basic(el);
+            let s = "_ptr_".to_owned() + get_type_name(bel.to_owned()).as_str();
+            s
+        }
+        _ => todo!(),
+    }
+}
+impl<'ctx> Source<'ctx> {
+    pub fn conv_into(
+        &mut self,
+        from: BasicValueEnum<'ctx>,
+        into: BasicTypeEnum<'ctx>,
+    ) -> Option<BasicValueEnum<'ctx>> {
+        if from.get_type() == into {
+            return Some(from);
+        }
+
+        match from.get_type() {
+            BasicTypeEnum::FloatType(_) => {
+                if !into.is_int_type() {
+                    // todo err here
+                    return None;
+                }
+                return Some(
+                    self.builder
+                        .build_float_to_signed_int(
+                            from.into_float_value(),
+                            into.into_int_type(),
+                            "fcon",
+                        )
+                        .unwrap()
+                        .as_basic_value_enum(),
+                );
+            }
+
+            BasicTypeEnum::IntType(_) => {
+                if !into.is_float_type() {
+                    return None;
+                }
+
+                return Some(
+                    self.builder
+                        .build_signed_int_to_float(
+                            from.into_int_value(),
+                            into.into_float_type(),
+                            "icon",
+                        )
+                        .unwrap()
+                        .as_basic_value_enum(),
+                );
+            }
+            _ => {
+                self.err(
+                    ErrKind::CannotConvertRight,
+                    "cannot convert right to left (usually in binary expressions)".to_string(),
+                );
+
+                None
+            } // err
+        }
+    }
+}
