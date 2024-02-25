a0c48e0cb650645d2d3323548d1efdd1b1cb54e7
Roxve <116169309+Roxve@users.noreply.github.com>
2024-02-25 19:04:28 +0000

    ir added

--- src/ir/gen.rs
+++ src/ir/gen.rs
@@ -1,14 +1,13 @@
-use super::{Const, ConstType, IROp};
+use super::{get_ops_type, Const, ConstType, IROp};
 use crate::{
     ast::{Expr, Literal},
     source::Source,
 };
 
 type IR = Vec<IROp>;
-type IRRes = Result<&mut IR, u8>;
+type IRRes = Result<IR, u8>;
 
 pub trait IRGen {
-    fn gen(&mut self, op: &mut IR) -> IRRes;
     fn gen_prog(&mut self, exprs: Vec<Expr>) -> IR;
     fn gen_expr(&mut self, expr: Expr) -> IRRes;
 
@@ -16,24 +15,21 @@ pub trait IRGen {
 }
 
 impl IRGen for Source {
-    fn gen(&mut self, ops: &mut IR) -> IRRes {
-        self.IR.append(ops);
-        Ok(ops)
-    }
     fn gen_prog(&mut self, exprs: Vec<Expr>) -> IR {
         for expr in exprs {
-            let _ = self.gen_expr(expr);
+            let g = self.gen_expr(expr);
+            if g.is_ok() {
+                self.IR.append(&mut g.unwrap());
+            }
         }
         self.IR.clone()
     }
 
     fn gen_expr(&mut self, expr: Expr) -> IRRes {
         match expr {
-            Expr::Literal(Literal::Int(i)) => {
-                Ok(&mut vec![IROp::Const(ConstType::Int, Const::Int(i))])
-            }
+            Expr::Literal(Literal::Int(i)) => Ok(vec![IROp::Const(ConstType::Int, Const::Int(i))]),
             Expr::Literal(Literal::Float(f)) => {
-                Ok(&mut vec![IROp::Const(ConstType::Float, Const::Float(f))])
+                Ok(vec![IROp::Const(ConstType::Float, Const::Float(f))])
             }
             Expr::BinaryExpr(op, left, right) => self.gen_binary_expr(op, *left, *right),
             _ => todo!(),
@@ -41,11 +37,38 @@ impl IRGen for Source {
     }
 
     fn gen_binary_expr(&mut self, op: String, left: Expr, right: Expr) -> IRRes {
-        self.gen_expr(left)?;
-        self.gen_expr(right)?;
-        match op.as_str() {
-            "+" => self.gen(IROp::Add),
-            o => todo!("add op {}", o),
+        let mut res: IR = vec![];
+        let mut lhs = self.gen_expr(left)?;
+        let mut rhs = self.gen_expr(right)?;
+        let ty;
+        if get_ops_type(&lhs) != get_ops_type(&rhs) {
+            // beform type conv
+            let lhs_ty = get_ops_type(&lhs);
+            let rhs_ty = get_ops_type(&rhs);
+            if lhs_ty == ConstType::Float && rhs_ty == ConstType::Int {
+                res.append(&mut lhs);
+                res.append(&mut rhs);
+                res.append(&mut vec![IROp::Conv(ConstType::Float)]);
+                ty = lhs_ty;
+            } else if lhs_ty == ConstType::Int && rhs_ty == ConstType::Float {
+                res.append(&mut lhs);
+                res.append(&mut vec![IROp::Conv(ConstType::Float)]);
+                res.append(&mut rhs);
+                ty = rhs_ty;
+            } else {
+                // NaN
+                ty = lhs_ty;
+            }
+        } else {
+            ty = get_ops_type(&lhs);
+            res.append(&mut lhs);
+            res.append(&mut rhs);
         }
+
+        res.append(&mut vec![match op.as_str() {
+            "+" => IROp::Add(ty),
+            o => todo!("add op {}", o),
+        }]);
+        Ok(res)
     }
 }
--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -2,10 +2,11 @@ pub mod gen;
 pub mod tools;
 
 #[derive(Debug, Clone, PartialEq)]
+#[repr(u8)]
 pub enum ConstType {
-    Int = 0,
-    Float = 2,
-    Str = 3,
+    Int = 0u8,
+    Float = 2u8,
+    Str = 3u8,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -17,15 +18,29 @@ pub enum Const {
 #[derive(Debug, Clone, PartialEq)]
 pub enum IROp {
     Def(String, Vec<IROp>),
-    Ret,
-    iAdd,
-    iSub,
-    iMul,
-    iDiv,
-    fAdd,
-    fSub,
-    fMul,
-    fDiv,
+    Ret(ConstType),
+    Add(ConstType),
+    Sub(ConstType),
+    Mul(ConstType),
+    Div(ConstType),
     Const(ConstType, Const),
     Conv(ConstType),
+}
+
+use self::IROp::*;
+pub fn get_op_type(op: &IROp) -> ConstType {
+    match op {
+        Def(_, ops) => get_op_type(ops.last().unwrap()),
+        Ret(t) => t.clone(),
+        Add(t) => t.clone(),
+        Sub(t) => t.clone(),
+        Mul(t) => t.clone(),
+        Div(t) => t.clone(),
+        Const(t, _) => t.clone(),
+        Conv(t) => t.clone(),
+    }
+}
+
+pub fn get_ops_type(ops: &Vec<IROp>) -> ConstType {
+    get_op_type(ops.last().unwrap())
 }
