2ac2150ead166afa3e1323beda68aaed3107968b
Roxve <116169309+Roxve@users.noreply.github.com>
2024-02-29 07:11:39 +0000

    working start func

--- src/backend/wasm/mod.rs
+++ src/backend/wasm/mod.rs
@@ -1,18 +1,37 @@
-use wasm_encoder::{CodeSection, Module};
+use wasm_encoder::{CodeSection, ExportKind, ExportSection, FunctionSection, Module, TypeSection};
 
 use crate::ir::{Const, ConstType, IROp};
+#[derive(Debug, Clone)]
+pub struct Section {
+    types: TypeSection,
+    code: CodeSection,
+    func: FunctionSection,
+    exports: ExportSection,
+}
 
+impl Section {
+    pub fn new() -> Self {
+        Section {
+            types: TypeSection::new(),
+            code: CodeSection::new(),
+            func: FunctionSection::new(),
+            exports: ExportSection::new(),
+        }
+    }
+}
 pub struct Codegen {
     current: wasm_encoder::Function,
     module: Module,
-    section: CodeSection,
+    section: Section,
     ir: Vec<IROp>,
     ip: usize,
 }
 impl Codegen {
     pub fn new(ir: Vec<IROp>) -> Self {
+        let mut section = Section::new();
+        let _ty = section.types.function(vec![], vec![]);
+        let _func = section.func.function(0);
         let _start = wasm_encoder::Function::new(vec![]);
-        let section = CodeSection::new();
         let module = Module::new();
         Codegen {
             current: _start,
@@ -34,6 +53,16 @@ impl Codegen {
                 _ => todo!(),
             }
         }
-        self.module.section(self.section.function(&self.current))
+        self.current.instruction(&wasm_encoder::Instruction::Return);
+
+        self.current.instruction(&wasm_encoder::Instruction::End);
+        self.module.section(&self.section.types);
+        self.module.section(&self.section.func);
+
+        self.section.exports.export("_start", ExportKind::Func, 0);
+        self.module.section(&self.section.exports);
+
+        self.module
+            .section(self.section.code.function(&self.current))
     }
 }
