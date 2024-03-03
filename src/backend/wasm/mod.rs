7c21974df930b32354f97fe285fabca95448bfdf
Roxve <116169309+Roxve@users.noreply.github.com>
2024-03-03 21:25:18 +0000

    restruct

--- src/backend/wasm/mod.rs
+++ src/backend/wasm/mod.rs
@@ -1,7 +1,9 @@
+pub mod codegen;
+
 use std::collections::HashMap;
 
 use wasm_encoder::{
-    CodeSection, ExportKind, ExportSection, Function, FunctionSection, Instruction, MemArg,
+    CodeSection, EntityType, ExportSection, Function, FunctionSection, ImportSection, Instruction,
     MemorySection, MemoryType, Module, TypeSection, ValType,
 };
 
@@ -14,6 +16,7 @@ pub struct Section {
     code: CodeSection,
     func: FunctionSection,
     mem: MemorySection,
+    imports: ImportSection,
     exports: ExportSection,
 }
 
@@ -23,6 +26,18 @@ impl Section {
             types: TypeSection::new(),
             code: CodeSection::new(),
             func: FunctionSection::new(),
+            imports: ImportSection::new()
+                .import(
+                    "mem",
+                    "memory",
+                    EntityType::Memory(MemoryType {
+                        minimum: 1,
+                        maximum: None,
+                        memory64: false,
+                        shared: false,
+                    }),
+                )
+                .to_owned(),
             exports: ExportSection::new(),
             mem: MemorySection::new()
                 .memory(MemoryType {
@@ -86,178 +101,4 @@ pub struct Codegen<'a> {
     section: Section,
     ir: Vec<IROp>,
     ip: usize,
-}
-impl<'a> Codegen<'a> {
-    pub fn new(ir: Vec<IROp>) -> Self {
-        let mut section = Section::new();
-        let _ty = section.types.function(vec![], vec![]);
-        let _func = section.func.function(0);
-        let module = Module::new();
-        Codegen {
-            current: Func::new(),
-            funcs: HashMap::new(),
-            section,
-            module,
-            ir,
-            ip: 0,
-        }
-    }
-
-    fn insert(&mut self, inst: Instruction<'a>) {
-        self.current.body.push(inst);
-        self.ip += 1;
-    }
-
-    fn insertp(&mut self, inst: Instruction<'a>) {
-        self.current.body.push(inst);
-    }
-    pub fn codegen(&mut self) -> &mut Module {
-        while self.ip <= self.ir.len() - 1 {
-            self.bond(self.ir[self.ip].clone());
-        }
-        self.insert(Instruction::Return);
-
-        self.insert(Instruction::End);
-
-        self.module.section(&self.section.types);
-        self.module.section(&self.section.func);
-
-        self.module.section(&self.section.mem);
-        self.section.exports.export("_start", ExportKind::Func, 0);
-        self.module.section(&self.section.exports);
-
-        // finnaly adding all functions indx by order
-
-        // _start
-        self.section.code.function(&self.current.finish());
-
-        let mut funcs_ordered: Vec<(u32, Func)> = self.funcs.clone().into_values().collect();
-        funcs_ordered.sort_by_key(|k| k.0);
-
-        for (_, mut func) in funcs_ordered {
-            self.section.code.function(&func.finish());
-        }
-
-        self.module.section(&self.section.code)
-    }
-
-    pub fn bond(&mut self, op: IROp) {
-        match op {
-            IROp::Const(ConstType::Int, Const::Int(i)) => self.insert(Instruction::I32Const(i)),
-            IROp::Const(ConstType::Float, Const::Float(f)) => {
-                self.insert(Instruction::F32Const(f));
-            }
-            IROp::Add(_) | IROp::Mul(_) | IROp::Div(_) | IROp::Sub(_) => {
-                return self.bond_binary_atoms(op);
-            }
-            IROp::Alloc(ty, name) => {
-                self.current.add_var(name, ty.into_val_type());
-                self.ip += 1;
-            }
-            IROp::Dealloc(_, _) => self.insert(Instruction::Nop),
-            IROp::Store(_, name) => {
-                let idx = self.current.get_var(name);
-                self.insert(Instruction::LocalSet(idx));
-            }
-            IROp::Load(_, name) => {
-                let idx = self.current.get_var(name);
-                self.insert(Instruction::LocalGet(idx));
-            }
-            IROp::Ret(_) => {
-                self.insert(Instruction::End);
-            }
-            IROp::Def(ty, name, args, body) => {
-                self.bond_func_atoms(ty.unwrap_or(ConstType::Void), name, args, body);
-            }
-            IROp::Conv(into, from) => {
-                match into {
-                    ConstType::Dynamic => match from {
-                        ConstType::Int => {
-                            self.insert(Instruction::I32Store(MemArg {
-                                offset: 1,
-                                align: 2,
-                                memory_index: 0,
-                            }));
-                            // first we write the type
-                            let idx = self.current.get_var("alloc".to_string());
-                            self.insertp(Instruction::LocalGet(idx));
-                            self.insertp(Instruction::I32Const(TYPE_INT));
-                            self.insertp(Instruction::I32Store8(MemArg {
-                                offset: 0,
-                                align: 0,
-                                memory_index: 0,
-                            }));
-                            self.insertp(Instruction::LocalGet(idx));
-                        }
-                        _ => todo!("add conv {:?} into dynamic", from),
-                    },
-                    _ => todo!("add whole conv {:?}", into),
-                }
-            }
-            IROp::Call(_, name) => {
-                let idx = self.funcs.get(&name).unwrap().0;
-                dbg!(&idx);
-                self.insert(Instruction::Call(idx));
-            }
-
-            _ => todo!("op not implented for the wasm backend {:?}", op),
-        }
-    }
-
-    pub fn bond_binary_atoms(&mut self, op: IROp) {
-        match op {
-            IROp::Add(ty) => match ty {
-                ConstType::Int => self.insert(Instruction::I32Add),
-                ConstType::Float => self.insert(Instruction::F32Add),
-                _ => todo!("add + for type {:?}", ty),
-            },
-            _ => todo!("add operator {:?}", op),
-        }
-    }
-
-    pub fn bond_func_atoms(
-        &mut self,
-        ty: ConstType,
-        name: String,
-        args: Vec<String>,
-        body: Vec<IROp>,
-    ) {
-        let mut func = Func::new();
-
-        // all args are dynamic for now
-        for arg in &args {
-            func.add_var(arg.to_owned(), ValType::I32);
-        }
-        let params: Vec<ValType> = args.iter().map(|_| ValType::I32).collect();
-
-        let old = self.current.clone();
-        let old_ip = self.ip;
-        let old_instr = self.ir.clone();
-
-        self.current = func;
-        self.ir = body;
-        dbg!(&self.ir);
-        self.ip = 0;
-
-        // EXECUTE CODEGEN!!!!!
-
-        while self.ip <= self.ir.len() - 1 {
-            self.bond(self.ir[self.ip].clone());
-        }
-
-        let func_type = self
-            .section
-            .types
-            .function(params, vec![ty.into_val_type()])
-            .len()
-            - 1;
-        self.funcs
-            .insert(name, (func_type.clone() as u32, self.current.clone()));
-
-        let _ = self.section.func.function(func_type);
-        // retreat fr
-        self.current = old;
-        self.ir = old_instr;
-        self.ip = old_ip + 1;
-    }
 }
