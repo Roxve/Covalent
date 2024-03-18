919eefcd5329d36e1c65f869e1247ac7da482671
Roxve <rox@localhost.localdomain>
2024-03-18 01:16:33 +0000

    cleaner code 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,123 @@
+use std::env;
+use std::fs;
+use std::process::Command;
 pub mod ast;
 pub mod backend;
 pub mod ir;
 pub mod lexer;
 pub mod parser;
 pub mod source;
+use crate::ast::Expr;
+use crate::backend::wasm;
+use crate::ir::gen::IRGen;
+use crate::parser::Parser;
+use crate::source::Source;
+
+#[allow(unused)]
+pub enum Backend {
+    WASM,
+    C,
+    Custom(String),
+}
+
+#[allow(unused)]
+pub struct CSettings {
+    compiler: Option<String>,
+    flags: Vec<String>,
+}
+
+impl CSettings {
+    pub fn new(compiler: Option<String>, flags: Vec<String>) -> Self {
+        Self { compiler, flags }
+    }
+}
+
+pub enum BackendSettings {
+    WASM,
+    C(CSettings),
+    Custom(Vec<String>),
+}
+pub struct CompilerConfig {
+    input: String,
+    backend: Backend,
+    settings: BackendSettings,
+    debug: bool,
+    repl: bool,
+}
+impl CompilerConfig {
+    pub fn new(
+        input: String,
+        backend: Backend,
+        settings: BackendSettings,
+        debug: bool,
+        repl: bool,
+    ) -> Self {
+        Self {
+            input,
+            backend,
+            settings,
+            debug,
+            repl,
+        }
+    }
+    pub fn run(&self) {
+        let mut src = Source::new(self.input.clone());
+
+        let prog: Vec<Expr> = src.parse_prog();
+        if self.debug {
+            println!("parsed prog:\n {:#?}\nsrc: \n{:#?}", prog, src);
+        }
+        let ir = src.gen_prog(prog);
+        dbg!(&ir);
+        let mut codegen = wasm::Codegen::new(ir);
+        let module = codegen.codegen();
+        dbg!(&module);
+        let bytes = module.clone().finish();
+        let path = "/tmp/test.wasm";
+        let _ = fs::write(path, bytes);
+
+        match self.backend {
+            Backend::WASM => {
+                // generate relocs
+                let _ = Command::new("wasm2wat")
+                    .arg(path)
+                    .arg("-o")
+                    .arg(format!("{}.wat", path))
+                    .spawn()
+                    .unwrap()
+                    .wait();
+                let _ = Command::new("wat2wasm")
+                    .arg("--relocatable")
+                    .arg(format!("{}.wat", path))
+                    .arg("-o")
+                    .arg(path)
+                    .spawn()
+                    .unwrap()
+                    .wait();
+
+                let libdir = env::current_exe()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .replace("covalent", "lib");
+                // links with std runtime mem
+                let _ = Command::new("wasm-ld")
+                    .arg("--relocatable")
+                    .arg(format!("{}/{}", libdir, "std.wasm"))
+                    .arg(format!("{}/{}", libdir, "runtime.wasm"))
+                    .arg(format!("{}/{}", libdir, "mem.wasm"))
+                    .arg(path)
+                    .arg("-o")
+                    .arg(path)
+                    .spawn()
+                    .unwrap()
+                    .wait();
+                if self.repl {
+                    let bytes = fs::read(path).unwrap();
+                }
+            }
+
+            _ => todo!(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,74 +7,30 @@ mod parser;
 mod source;
 
 use std::path::Path;
-use std::process::Command;
+// use std::process::Command;
 use std::{env, fs};
 
-use ir::gen::IRGen;
+use covalent::CompilerConfig;
 
-use crate::ast::*;
-use crate::backend::wasm::*;
-use crate::parser::*;
-use crate::source::*;
+// use ir::gen::IRGen;
+
+// use crate::ast::*;
+// use crate::backend::wasm::*;
+// use crate::parser::*;
+// use crate::source::*;
+
 #[test]
 fn test() {
     let prog = fs::read_to_string("TestProg/main.atoms").unwrap();
-    run(prog, true, false, "test".to_string());
-}
 
-fn run(input: String, is_debug: bool, is_repl: bool, name: String) {
-    let mut src = Source::new(input);
-
-    let prog: Vec<Expr> = src.parse_prog();
-    if is_debug {
-        println!("parsed prog:\n {:#?}\nsrc: \n{:#?}", prog, src);
-    }
-    let ir = src.gen_prog(prog);
-    dbg!(&ir);
-    let mut codegen = Codegen::new(ir);
-    let module = codegen.codegen();
-    dbg!(&module);
-    let bytes = module.clone().finish();
-    let path = "/tmp/test.wasm";
-    let _ = fs::write(path, bytes);
-
-    // generate relocs
-    let _ = Command::new("wasm2wat")
-        .arg(path)
-        .arg("-o")
-        .arg(format!("{}.wat", path))
-        .spawn()
-        .unwrap()
-        .wait();
-    let _ = Command::new("wat2wasm")
-        .arg("--relocatable")
-        .arg(format!("{}.wat", path))
-        .arg("-o")
-        .arg(path)
-        .spawn()
-        .unwrap()
-        .wait();
-
-    let libdir = env::current_exe()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .replace("covalent", "lib");
-    // links with std runtime mem
-    let _ = Command::new("wasm-ld")
-        .arg("--relocatable")
-        .arg(format!("{}/{}", libdir, "std.wasm"))
-        .arg(format!("{}/{}", libdir, "runtime.wasm"))
-        .arg(format!("{}/{}", libdir, "mem.wasm"))
-        .arg(path)
-        .arg("-o")
-        .arg(path)
-        .spawn()
-        .unwrap()
-        .wait();
-    if is_repl {
-        let bytes = fs::read(path).unwrap();
-    }
+    CompilerConfig::new(
+        prog,
+        covalent::Backend::WASM,
+        covalent::BackendSettings::WASM,
+        true,
+        true,
+    )
+    .run();
 }
 
 fn repl(is_debug: bool) {
@@ -83,16 +39,20 @@ fn repl(is_debug: bool) {
 
     io::stdout().flush().unwrap();
 
-    let _ = stdin.read_line(&mut buffer);
-
     println!("running repl...(VERY WIP PLEASE COMPILE FILE INSTEAD)");
     loop {
         print!(">> ");
         io::stdout().flush().unwrap();
 
-        let mut repl = String::with_capacity(4096);
-        let _ = stdin.read_line(&mut repl);
-        run(repl, is_debug, true, "repl".to_string());
+        let _ = stdin.read_line(&mut buffer);
+        CompilerConfig::new(
+            buffer.clone(),
+            covalent::Backend::WASM,
+            covalent::BackendSettings::WASM,
+            true,
+            true,
+        )
+        .run();
     }
 }
 
@@ -124,10 +84,12 @@ fn main() {
     let path = Path::new(file.as_str());
 
     let filename = path.file_name().expect("file passed is a folder");
-    run(
-        prog.expect("file doesnt exist"),
+    CompilerConfig::new(
+        prog.expect("invaild file name"),
+        covalent::Backend::WASM,
+        covalent::BackendSettings::WASM,
         is_debug,
         false,
-        filename.to_str().unwrap().to_string().replace(".atoms", ""),
     )
+    .run();
 }
